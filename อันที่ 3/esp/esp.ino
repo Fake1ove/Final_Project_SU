cb480d439ceb4a95f9af9455b13d617cfd42a0fd
Fake1ove <teebenten10@gmail.com>
2025-04-12 04:00:43 +0700

    Delete อันที่ 3 directory

--- อันที่ 3/esp/esp.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  Serial.begin(115200);
-}
-void loop() {
-  Serial.print("1"); // turn the LED on
-  delay(1000); // wait for a second
-  Serial.print("0"); // turn the LED off
-  delay(1000); // wait for a second
-}
